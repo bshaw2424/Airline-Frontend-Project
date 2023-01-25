c419384699f1240ad3188c255c0030b46db7f545
Bishop Shaw III <prototype124@yahoo.com>
2023-01-24 21:51:26 -0700

    add the airlines landing page

--- src/components/Airlines.js
+++ src/components/Airlines.js
@@ -1,17 +1,45 @@
 import axios from "axios";
-import { Link, Outlet, useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 
 export default function Airlines() {
   const airline = useLoaderData();
   return (
-    <>
-      {airline.map(airline => (
-        <div key={airline._id}>
-          <Link to={`/airlines/${airline.slug}`}>{airline.name}</Link>
-        </div>
-      ))}
-      <Outlet />
-    </>
+    <div className="container">
+      <div className="my-1 p-3" style={{ width: "100%" }}>
+        <h1 className="text-center p-3">Browse Airline Destinations</h1>
+        <p>
+          <strong>Disclaimer: </strong>Airline names and/or destinations subject
+          to change at any time. We dont have any affiliation with any airlines.
+          Some airlines may operate under subsidary airlines which can have more
+          destinations. Visit your favorite airline's website for any additional
+          information.
+        </p>
+      </div>
+      <div className="row">
+        {airline.map(airline => (
+          <div key={airline._id} className="col-xs-6 col-sm-6 mb-4">
+            <Link to={`/airlines/${airline.slug}`}>
+              <div
+                className="card"
+                style={{
+                  alignItems: "center",
+                  padding: "4px",
+                }}
+              >
+                <div className="card-body">
+                  <h2>{airline.name}</h2>
+                  <h5 style={{ margin: "0" }}>
+                    <strong>Destinations: </strong>
+
+                    {airline.destinations.length}
+                  </h5>
+                </div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
