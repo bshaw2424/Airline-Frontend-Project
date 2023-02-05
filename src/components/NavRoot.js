a6daaccf5c777cd01e635da3e9a1d60f3ac12b14
Bishop Shaw III <prototype124@yahoo.com>
2023-02-04 19:23:28 -0700

    add footer to navrooot

--- src/components/NavRoot.js
+++ src/components/NavRoot.js
@@ -1,19 +1,34 @@
 import { Link, NavLink, Outlet } from "react-router-dom";
+import Footer from "./Footer";
 
 export default function NavRoot() {
   return (
     <header>
-      <nav>
-        <h1>
-          <Link to="/">LOGO</Link>
-        </h1>
-        <div>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/airlines">Airlines</NavLink>
-        </div>
-      </nav>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          background: "blue",
+        }}
+      >
+        <nav className="container nav d-flex justify-content-between align-items-center py-2">
+          <h1>
+            <Link to="/" className="navbar-brand">
+              LOGO
+            </Link>
+          </h1>
+          <div className="d-flex">
+            <NavLink Link to="/" className="mx-2">
+              Home
+            </NavLink>
 
+            <NavLink to="/airlines">Airlines</NavLink>
+          </div>
+        </nav>
+      </div>
       <Outlet />
+      <Footer />
     </header>
   );
 }
