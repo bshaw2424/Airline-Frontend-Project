41a46946065737563ecd2d661a46b9eb10b12ca7
Bishop Shaw III <prototype124@yahoo.com>
2023-02-07 21:18:35 -0700

    added footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="py-3" style={{ background: "tan" }}>
+      <div
+        style={{ outline: "3px solid green" }}
+        className="container d-flex justify-content-center align-item-center"
+      >
+        <p>CopyRight 2023 All Rights Reserved</p>
+      </div>
+    </footer>
+  );
+}
