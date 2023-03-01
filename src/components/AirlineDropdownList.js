01bb11bdd608c0b03815b2ee285520e6853f27dc
Bishop Shaw III <prototype124@yahoo.com>
2023-02-28 22:07:24 -0700

    airline dropdown list component

--- src/components/AirlineDropdownList.js
+++ src/components/AirlineDropdownList.js
@@ -1,30 +1,62 @@
-import SideNav from "./SideNav";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
 
-export default function AirlineDropdownList({ destinations }) {
-  const [airlineNames, setAirlineNames] = useState([destinations]);
+export default function AirlineDropdownList({
+  dropDownName,
+  stateLocation,
+  mainPage,
+}) {
+  const [airlines, setAirlines] = useState([]);
 
   useEffect(() => {
     axios
-      .get("http://localhost:8080/airlines")
-      .then(res => {
-        setAirlineNames(res.data);
+      .get(`http://localhost:8080/airlines`)
+      .then(response => {
+        setAirlines(response.data);
       })
-      .catch(error => console.log(error));
+      .catch(e => console.log(e.message));
   }, []);
 
+  function value(e) {
+    const getAirlineDestinationsLengths = airlines
+      .filter(
+        names =>
+          names.slug === e.target.innerText.replace(/ /g, "-").toLowerCase(),
+      )
+      .map(
+        airlineDestinationsLength =>
+          airlineDestinationsLength.destinations.length,
+      );
+    mainPage(true);
+    stateLocation(getAirlineDestinationsLengths);
+  }
+
   return (
-    <span className="dropdown ms-5">
+    <div className="dropdown">
       <button
-        className="btn dropdown-toggle"
+        className="btn  dropdown-toggle"
         type="button"
         data-bs-toggle="dropdown"
         aria-expanded="false"
       >
-        Change Airlines
+        {dropDownName}
       </button>
-      <SideNav airline={airlineNames} />
-    </span>
+
+      <ul className="dropdown-menu">
+        {airlines.map(airlineNames => (
+          <li key={airlineNames._id}>
+            <Link
+              onClick={e => value(e)}
+              to={`/airlines/${airlineNames.slug}`}
+              className="dropdown-item"
+              value={airlineNames.name}
+            >
+              {airlineNames.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
