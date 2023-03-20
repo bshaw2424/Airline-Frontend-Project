25d7941bc4309f08dd00c989d24df57765c4f63c
Bishop Shaw III <prototype124@yahoo.com>
2023-03-19 21:25:44 -0700

    Destinations component file

--- src/components/Destinations.js
+++ src/components/Destinations.js
@@ -1,44 +1,21 @@
-import { Link, useLoaderData, useParams } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
 import axios from "axios";
+import AirlineDisplayContainer from "./AirlineDisplayContainer";
+import DestinationCard from "./DestinationCard";
 
 export default function Destinations() {
   const { slug } = useParams();
   const destinations = useLoaderData();
 
   return (
-    <div>
-      <h1>
-        {destinations.name} - {destinations.destinations.length}
-      </h1>
+    <>
+      <AirlineDisplayContainer
+        destinations={destinations}
+        stateDestinations={destinations}
+      />
 
-      <section
-        style={{
-          display: "flex",
-          width: "50%",
-          flexWrap: "wrap",
-          margin: "4rem auto",
-          justifyContent: "space-between",
-          gap: "1rem",
-        }}
-      >
-        {destinations.destinations.map(items => (
-          <div
-            style={{
-              outline: "3px solid green",
-              width: "40%",
-            }}
-          >
-            {items.name} - <b>{items.airport_code}</b>
-            <p>
-              <b>Location:</b> {items.city}, {items.state}
-            </p>
-            <p>
-              <b>Airport:</b> {items.airport_name}
-            </p>
-          </div>
-        ))}
-      </section>
-    </div>
+      {/* <DestinationCard destinations={destinations} /> */}
+    </>
   );
 }
 
@@ -46,8 +23,6 @@ export const destinationsLoader = async ({ params }) => {
   const { slug } = params;
 
   const response = await axios.get(`http://localhost:8080/airlines/${slug}`);
-
-  console.log(response.data);
 
   return response.data;
 };
