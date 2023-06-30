5db3cd9b6c0dd21443f5adcf1e60c7de80eb6907
Bishop Shaw III <prototype124@yahoo.com>
2023-06-29 21:24:21 -0700

    updated Home.js component

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,6 @@ export default function Home() {
     <main>
       <div
         style={{
-          height: "70vh",
           display: "flex",
           justifyContent: "space-evenly",
           alignItems: "center",
@@ -14,45 +13,84 @@ export default function Home() {
       >
         <div
           style={{
+            height: "90vh",
             display: "flex",
+            justifyContent: "space-evenly",
             alignItems: "center",
-            flexDirection: "column",
-            height: "100%",
-            justifyContent: "center",
-            margin: "0 0 0 3rem",
+            flexDirection: "row",
+            borderTop: "2px solid green",
+            marginTop: "0",
           }}
-          className="container"
+          className="border-top border-bottom border-top-1 border-bottom-1 border-light"
         >
-          <h2>Title Goes Here</h2>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed, ipsa
-            libero! Sapiente commodi distinctio fugit a exercitationem dolorem
-            reprehenderit beatae similique veritatis quisquam excepturi eveniet
-            numquam aspernatur, qui quod possimus.
-          </p>
-          <Link className="btn btn-primary" to="/airlines">
-            View Airlines
-          </Link>
-        </div>
-        <div
-          style={{
-            height: "100%",
-            width: "100%",
-            objectFit: "contain",
-            border: "1px soild black",
-          }}
-          className="container"
-        >
-          <img
+          <div
             style={{
-              objectFit: "cover",
-              height: "100%",
-              Width: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyItems: "center",
+              flexDirection: "column",
+              justifyContent: "end",
+              width: "100%",
+              height: "75%",
             }}
-            src="airport.jpg"
-            alt="pch.vector on Freepik"
-          />
+            className="container"
+          >
+            <h2
+              style={{ fontSize: "3rem", width: "90%" }}
+              className="pb-2 border-bottom border-bottom-1 border-dark  text-center py-2"
+            >
+              Let The Journey Begin
+            </h2>
+            <div
+              style={{
+                height: "75%",
+                paddingLeft: "3rem",
+              }}
+              className="container pt-2"
+            >
+              <p style={{ fontSize: "1.2rem" }} className="">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed,
+                ipsa libero! Sapiente commodi distinctio fugit a exercitationem
+                dolorem reprehenderit beatae similique veritatis quisquam
+                excepturi eveniet numquam aspernatur, qui quod possimus.
+              </p>
+              <div className="d-flex justify-content-end my-3">
+                <Link
+                  style={{ width: "30%" }}
+                  className="btn btn-primary"
+                  to="/airlines"
+                >
+                  View Airlines
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{
+              height: "76%",
+              width: "100%",
+              objectFit: "contain",
+            }}
+            className="container d-flex justify-content-start align-items-center "
+          >
+            <img
+              style={{
+                objectFit: "contain",
+                height: "100%",
+                Width: "90%",
+              }}
+              className="rounded"
+              src="airport.jpg"
+              alt="pch.vector on Freepik"
+            />
+          </div>
         </div>
+      </div>
+      <div
+        style={{ height: "100vh" }}
+        className="border border-top border-top-1 border-top-dark"
+      >
+        <p>Hello World Second Section</p>
       </div>
     </main>
   );
