251a45812a553adb1152159eddde0cdce3a4d4a5
Bishop Shaw III <prototype124@yahoo.com>
2023-07-19 18:10:02 -0700

    fix some functionality in the Form conponent

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,42 @@
+import { useState } from "react";
+import { unstable_HistoryRouter } from "react-router-dom";
+
 export default function Form({
   value,
   airlineSearchSubmit,
-  changeSelectItems,
   clearInputField,
+  inputField,
 }) {
+  function keyDownEvent() {
+    document.querySelector("#stateDestinationMap");
+    let input = document.querySelector("#airportDataInput");
+    let scrollPosition = input.value.length !== 0 ? 515 : null;
+    if (inputField !== "airport_code") {
+      window.scrollTo({ top: scrollPosition, behavior: "smooth" });
+    }
+  }
+  const formScollStyle = {
+    position: "sticky",
+    top: 0,
+    zIndex: 1,
+    background: "#fff",
+    width: "100%",
+  };
+
+  const none = {
+    position: "static",
+  };
+
   return (
-    <div>
-      <form onSubmit={airlineSearchSubmit}>
+    <div
+      className="p-3 mb-1 shadow-sm"
+      style={inputField === "airport_code" ? none : formScollStyle}
+    >
+      <form
+        onSubmit={airlineSearchSubmit}
+        className="py-2"
+        style={{ width: "100%" }}
+      >
         <div className="row d-flex justify-content-center">
           <div className="col-sm-3 d-flex">
             <label
@@ -17,7 +47,6 @@ export default function Form({
               }}
               htmlFor="search-options"
             ></label>
-
             <select
               style={{ maxWidth: "100%" }}
               name="airlineOptions"
@@ -42,8 +71,14 @@ export default function Form({
               required
             />
           </div>
-          <div className="col-sm-3">
-            <button type="submit" className="btn btn-primary">
+          <div className="col-sm-3" style={{ width: "10%" }}>
+            <button
+              href="#stateDestinationMap"
+              type="submit"
+              className="btn btn-primary"
+              onClick={() => keyDownEvent()}
+              style={{ width: "100%" }}
+            >
               Submit
             </button>
           </div>
