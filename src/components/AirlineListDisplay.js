e7f6568cf5cba51c4d8469862306eeed13c6017f
Bishop Shaw III <prototype124@yahoo.com>
2023-07-22 21:16:52 -0700

    updated the AirlineLineDisplay.js component

--- src/components/AirlineListDisplay.js
+++ src/components/AirlineListDisplay.js
@@ -1,43 +1,12 @@
-import { getFilteredAirportCodeOrCity } from "../Utilities";
-
-const displayMessage = {
-  show: "block",
-  hide: "none",
-  style: "none",
-  textSize: "1.2rem",
-};
-
-const grid = {
-  style: "grid",
-  columns: "repeat(3, auto)",
-};
-
-export default function AirlineListDisplay({
-  airlineSearch,
-  category,
-  targetCategoryValue,
-  count,
-}) {
+export default function AirlineListDisplay({ displayMessage }) {
   return (
-    <div className="my-3" style={{ width: "100%" }}>
-      <h2 className="text-center">{count}</h2>
-      <div
-        style={{
-          display: grid.style,
-          gridTemplateColumns: grid.columns,
-          paddingTop: "1rem",
-        }}
-      >
-        {airlineSearch.map(airline => (
-          <>
-            {getFilteredAirportCodeOrCity(
-              airline,
-              category,
-              targetCategoryValue,
-            )}
-          </>
-        ))}
-      </div>
+    <div
+      className="mt-4"
+      style={{
+        width: "100%",
+      }}
+    >
+      <h2 className="text-center">{displayMessage}</h2>
     </div>
   );
 }
