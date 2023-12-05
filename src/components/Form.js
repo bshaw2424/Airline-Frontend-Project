db41a328977b5678e987145a7ae8e5fb769e8eb8
Bishop Shaw III <prototype124@yahoo.com>
2023-12-04 19:14:20 -0700

    update from to add a button component. Still fixing bugs with in form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import { unstable_HistoryRouter } from "react-router-dom";
+import Button from "./Button";
 
 export default function Form({
   value,
@@ -7,48 +6,14 @@ export default function Form({
   clearInputField,
   inputField,
 }) {
-  function keyDownEvent() {
-    document.querySelector("#stateDestinationMap");
-    let input = document.querySelector("#airportDataInput");
-    let scrollPosition = input.value.length !== 0 ? 515 : null;
-    if (inputField !== "airport_code") {
-      window.scrollTo({ top: scrollPosition, behavior: "smooth" });
-    }
-  }
-  const formScollStyle = {
-    position: "sticky",
-    top: 0,
-    zIndex: 1,
-    background: "#fff",
-    width: "100%",
-  };
-
-  const none = {
-    position: "static",
-  };
-
   return (
-    <div
-      className="p-3 mb-1 shadow-sm"
-      style={inputField === "airport_code" ? none : formScollStyle}
-    >
-      <form
-        onSubmit={airlineSearchSubmit}
-        className="py-2"
-        style={{ width: "100%" }}
-      >
+    <div className="p-3 mb-1 shadow-sm">
+      <form onSubmit={airlineSearchSubmit} className="py-2">
         <div className="row d-flex justify-content-center">
+          {/* select dropdown menu for input field */}
           <div className="col-sm-3 d-flex">
-            <label
-              style={{
-                alignSelf: "center",
-                maxWidth: "100%",
-                fontSize: "1rem",
-              }}
-              htmlFor="search-options"
-            ></label>
+            <label htmlFor="search-options"></label>
             <select
-              style={{ maxWidth: "100%" }}
               name="airlineOptions"
               className="form-select p-2"
               aria-label="Default select example"
@@ -58,6 +23,8 @@ export default function Form({
               <option value="airport_code">Airport Code</option>
             </select>
           </div>
+
+          {/* input field */}
           <div className="col-sm-3 d-flex justify-content-center">
             <label htmlFor="airportDataInput"></label>
             <input
@@ -71,16 +38,15 @@ export default function Form({
               required
             />
           </div>
+
           <div className="col-sm-3" style={{ width: "10%" }}>
-            <button
-              href="#stateDestinationMap"
-              type="submit"
+            <Button
+              buttonType="submit"
               className="btn btn-primary"
-              onClick={() => keyDownEvent()}
-              style={{ width: "100%" }}
+              href="#stateDestinationMap"
             >
               Submit
-            </button>
+            </Button>
           </div>
         </div>
       </form>
