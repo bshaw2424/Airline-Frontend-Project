11da92e5b52a022481df7d3c1b49b5e5d5dd78e0
Bishop Shaw III <prototype124@yahoo.com>
2023-12-11 18:30:35 -0700

    updated with a new onchange function and cleaned up component by removing unnecessary code

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,44 +1,24 @@
+import AirlineQuickSearch from "./AirlineQuickSearch";
+
 import Button from "./Button";
+import SelectMenu from "./SelectMenu";
 
-export default function Form({
-  value,
-  airlineSearchSubmit,
-  clearInputField,
-  inputField,
-}) {
+export default function Form({ onSubmit, onChange, value }) {
   return (
-    <div className="p-3 mb-1 shadow-sm">
-      <form onSubmit={airlineSearchSubmit} className="py-2">
+    <div
+      className="p-3 mb-1 shadow-sm"
+      style={{
+        position: "sticky",
+        top: 0,
+        zIndex: 1,
+        background: "#fff",
+      }}
+    >
+      <form onSubmit={onSubmit} className="py-2">
         <div className="row d-flex justify-content-center">
-          {/* select dropdown menu for input field */}
-          <div className="col-sm-3 d-flex">
-            <label htmlFor="search-options"></label>
-            <select
-              name="airlineOptions"
-              className="form-select p-2"
-              aria-label="Default select example"
-              id="search-options"
-            >
-              <option value="state">State / Country </option>
-              <option value="airport_code">Airport Code</option>
-            </select>
-          </div>
-
-          {/* input field */}
-          <div className="col-sm-3 d-flex justify-content-center">
-            <label htmlFor="airportDataInput"></label>
-            <input
-              className="p-2"
-              style={{ alignSelf: "center", width: "100%", height: "100%" }}
-              type="text"
-              name="data"
-              id="airportDataInput"
-              onChange={clearInputField}
-              value={value}
-              required
-            />
-          </div>
-
+          {/* select dropdown menu to choose between state or airport code */}
+          <SelectMenu />
+          <AirlineQuickSearch value={value} onChange={onChange} />
           <div className="col-sm-3" style={{ width: "10%" }}>
             <Button
               buttonType="submit"
