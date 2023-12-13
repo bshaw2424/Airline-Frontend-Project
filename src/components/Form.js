5558a3e07c027e5ff334eb27b94e05c3bd4f33b1
Bishop Shaw III <prototype124@yahoo.com>
2023-12-12 19:43:08 -0700

    modified to simplify and added components to the form component

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,35 +1,53 @@
+import { useState } from "react";
 import AirlineQuickSearch from "./AirlineQuickSearch";
 
 import Button from "./Button";
 import SelectMenu from "./SelectMenu";
+import AirlineStateSearch from "./AirlineStateSearch";
 
-export default function Form({ onSubmit, onChange, value }) {
+export default function Form({ onSubmit, airlineLinks, state }) {
+  const [formValue, setFormValue] = useState("");
+  const [selectOption, setSelectOption] = useState();
+
+  function handleOptionChange(e) {
+    setSelectOption(e.target.value);
+  }
+  function formChange(e) {
+    setFormValue(e.target.value);
+  }
   return (
-    <div
-      className="p-3 mb-1 shadow-sm"
-      style={{
-        position: "sticky",
-        top: 0,
-        zIndex: 1,
-        background: "#fff",
-      }}
-    >
-      <form onSubmit={onSubmit} className="py-2">
-        <div className="row d-flex justify-content-center">
-          {/* select dropdown menu to choose between state or airport code */}
-          <SelectMenu />
-          <AirlineQuickSearch value={value} onChange={onChange} />
-          <div className="col-sm-3" style={{ width: "10%" }}>
-            <Button
-              buttonType="submit"
-              className="btn btn-primary"
-              href="#stateDestinationMap"
-            >
-              Submit
-            </Button>
+    <>
+      <div
+        className="p-3 mb-1 shadow-sm"
+        style={{
+          position: "sticky",
+          top: 0,
+          zIndex: 1,
+          background: "#fff",
+        }}
+      >
+        <form onSubmit={onSubmit} className="py-2">
+          <div className="row d-flex justify-content-center">
+            {/* select dropdown menu to choose between state or airport code */}
+            <SelectMenu onChange={e => handleOptionChange(e)} />
+            <AirlineQuickSearch
+              value={formValue}
+              onChange={e => formChange(e)}
+            />
+            <div className="col-sm-3" style={{ width: "10%" }}>
+              <Button
+                buttonType="submit"
+                className="btn btn-primary"
+                href="#stateDestinationMap"
+              >
+                Submit
+              </Button>
+            </div>
           </div>
-        </div>
-      </form>
-    </div>
+        </form>
+      </div>
+      {airlineLinks}
+      {selectOption === "state" && state}
+    </>
   );
 }
