feb3b00eae32b10d111abf406752be0daa550de9
Bishop Shaw III <prototype124@yahoo.com>
2024-01-12 19:49:21 -0700

    Created international search component. Still have to get it functional and working properly

---
+++ src/components/InternationalSearch.js
@@ -0,0 +1,63 @@
+import React, { useEffect } from "react";
+import DisclaimerMessage from "./DisclaimerMessage";
+import StateMap from "./StateMap";
+import StateList from "./StateList";
+
+export default function InternationalSearch({
+  airlineSearch,
+  targetCategoryValue,
+  airlineName,
+  isScrolled,
+}) {
+  useEffect(() => {
+    if (isScrolled) {
+      const element = document.getElementById("stateDestinationMap");
+
+      if (element) {
+        element.scrollIntoView({
+          alignToTop: true,
+          behavior: "smooth",
+        });
+      }
+    }
+  }, [isScrolled]);
+
+  const getListOfInternationalDestinations = airlineSearch
+    .map(internationalDestination => ({
+      name: internationalDestination.name,
+      codes: internationalDestination.destinations
+        .filter(location => location.state === targetCategoryValue)
+        .map(location => location.airport_code),
+    }))
+    .filter(listItem => listItem.codes.length !== 0)
+    .reduce((acc, a) => {
+      acc[a.name] = false;
+      return acc;
+    }, {});
+
+  return (
+    <article className="mt-5">
+      <div id="stateDestinationMap">
+        <div className="d-flex justify-content-evenly">
+          <StateList
+            dataList={airlineSearch}
+            searchValue={targetCategoryValue}
+            objectState={getListOfInternationalDestinations}
+          />
+
+          {/*  SHOWS CENTER POINT OF STATE USED INPUT */}
+          <StateMap
+            displayMap={airlineSearch}
+            centerPointOfMap={targetCategoryValue}
+          />
+        </div>
+      </div>
+      <div className="text-center pt-4">
+        <h2 style={{ fontSize: "2.4rem" }}>
+          {airlineName}
+          <DisclaimerMessage />
+        </h2>
+      </div>
+    </article>
+  );
+}
