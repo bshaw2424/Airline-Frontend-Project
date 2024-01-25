b199791e6bc82a5202b6c7f0f85c2f9d24c065bf
Bishop Shaw III <prototype124@yahoo.com>
2024-01-24 23:36:13 -0700

    add markers to the google maps feature when clicking on a airline name. Still need to adjust some features associated with it

--- src/components/StateList.js
+++ src/components/StateList.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { MarkerF } from "@react-google-maps/api";
+import { Marker, InfoWindow } from "@react-google-maps/api";
 import {
   changeAirportCodeToIcaoCode,
   axiosCallToLatitudeAndLongitudeCoordinates,
@@ -8,25 +8,6 @@ import {
 import StateMap from "./StateMap";
 
 export default function StateList({ dataList, searchValue, objectState }) {
-  const [getActiveState, setActiveState] = useState(objectState);
-  const [coords, setCoords] = useState();
-  const [airlineNameList, setAirlineNameList] = useState();
-  const [markerData, setMarkerData] = useState();
-
-  useEffect(() => {
-    axiosCallToLatitudeAndLongitudeCoordinates(searchValue);
-  }, [searchValue]);
-
-  const handleStateClick = airlineName => {
-    setActiveState(prevState => ({
-      ...Object.fromEntries(
-        Object.entries(prevState).map(([key]) => [key, false]),
-      ),
-      [airlineName]: true,
-    }));
-    getCoordinatesAndTitleToAddToMap(airlineName);
-  };
-
   const getListOfAirlinesObject = dataList
     .map(state => ({
       // airline name
@@ -52,36 +33,35 @@ export default function StateList({ dataList, searchValue, objectState }) {
     }))
     .filter(listItem => listItem.codes.length !== 0);
 
+  const [getActiveState, setActiveState] = useState(objectState);
+  const [airlineObjectData] = useState(getListOfAirlinesObject);
+  const [coords, setCoords] = useState();
+  const [airlineNameList, setAirlineNameList] = useState();
+  const [airlineIndex, setAirlineIndex] = useState();
+
+  useEffect(() => {
+    axiosCallToLatitudeAndLongitudeCoordinates(searchValue);
+  }, [searchValue]);
+
+  const handleStateClick = airlineName => {
+    setActiveState(prevState => ({
+      ...Object.fromEntries(
+        Object.entries(prevState).map(([key]) => [key, false]),
+      ),
+      [airlineName]: true,
+    }));
+    setAirlineIndex(getCoordinatesAndTitleToAddToMap(airlineName));
+  };
+
+  // gets the index of the airline name that was clicked
+  const getCoordinatesAndTitleToAddToMap = airlineName =>
+    airlineNameList.findIndex(name => name === airlineName);
+
   useEffect(() => {
     const getCoordinates = async () => {
       try {
-        const getListOfAirlinesObject = dataList
-          .map(state => ({
-            // airline name
-            name: state.name,
-            codes: state.destinations
-              .filter(location => location.state === searchValue)
-              // get object of ICAO airport codes and corresponding airport name
-              .reduce((createObjectOfAirportCodeAndName, items) => {
-                createObjectOfAirportCodeAndName.push({
-                  code: changeAirportCodeToIcaoCode(items.airport_code),
-                  airport: items.airport_name,
-                });
-                return createObjectOfAirportCodeAndName;
-              }, []),
-            // length of destinations within the targeted state search (ex... Arizona)
-            length: state.destinations
-              .filter(
-                location =>
-                  location.state === searchValue &&
-                  location.international === "false",
-              )
-              .map(location => location.airport_code).length,
-          }))
-          .filter(listItem => listItem.codes.length !== 0);
-
         const dataResultsObjectWithNameAndCoordinates = await Promise.all(
-          getUniqueListOfAirportCodes(getListOfAirlinesObject).map(
+          getUniqueListOfAirportCodes(airlineObjectData).map(
             async locationNameAndCoordinates => {
               const getLatitudeLongitudeCoordinatesFromAPI =
                 await axiosCallToLatitudeAndLongitudeCoordinates(
@@ -97,48 +77,51 @@ export default function StateList({ dataList, searchValue, objectState }) {
         );
 
         // puts the lat and lng coordinates in a object
-        const compareListToFindMatchingCodes = getListOfAirlinesObject.map(
-          coords =>
-            coords.codes.map(matchingName => {
-              const findMatchingName =
-                dataResultsObjectWithNameAndCoordinates.find(
-                  object => object.name === matchingName.code,
-                );
-              return findMatchingName
-                ? {
-                    location: findMatchingName.coordinates,
+        const compareListToFindMatchingCodes = airlineObjectData.map(airline =>
+          airline.codes.map(matchingName => {
+            const findMatchingName =
+              dataResultsObjectWithNameAndCoordinates.find(
+                object => object.name === matchingName.code,
+              );
+            return findMatchingName
+              ? {
+                  location: {
+                    ...findMatchingName.coordinates,
                     title: matchingName.airport,
-                  }
-                : matchingName;
-            }),
+                  },
+                }
+              : matchingName;
+          }),
         );
+        console.log(compareListToFindMatchingCodes);
 
-        const getListOfAirlineNames = getListOfAirlinesObject.map(a => a.name);
-        setAirlineNameList(getListOfAirlineNames);
-
+        const getArrayListOfAirlineNames = airlineObjectData.map(a => a.name);
+        setAirlineNameList(getArrayListOfAirlineNames);
         setCoords(compareListToFindMatchingCodes);
       } catch (error) {
         console.log("there was a error " + error);
       }
     };
     getCoordinates();
-  }, [dataList, searchValue]);
+  }, [dataList, searchValue, airlineObjectData]);
 
-  const getCoordinatesAndTitleToAddToMap = airlineName => {
-    const indexOfClickedAirlineName = airlineNameList.findIndex(
-      name => name === airlineName,
-    );
-    const getMapPositionCoordinates = coords[indexOfClickedAirlineName].map(
-      a => {
-        const {
-          location: { lat, lng },
-          title,
-        } = a;
-        return { lat: +lat, lng: +lng, title };
-      },
-    );
-    return setMarkerData(getMapPositionCoordinates);
-  };
+  const getMarkers =
+    coords &&
+    coords[airlineIndex] &&
+    coords[airlineIndex].map((coordinates, index) => {
+      const {
+        location: { lat, lng, title },
+      } = coordinates;
+      return (
+        <>
+          <Marker
+            key={`${index}`}
+            position={{ lat: +lat, lng: +lng }}
+            title={title}
+          />
+        </>
+      );
+    });
 
   return (
     <div
@@ -150,7 +133,7 @@ export default function StateList({ dataList, searchValue, objectState }) {
       }}
     >
       <ul style={{ listStyle: "none" }}>
-        {getListOfAirlinesObject.map((airline, index) => (
+        {airlineObjectData.map((airline, index) => (
           <li
             key={`${airline.name}-${index}`}
             className={`ps-4 py-1 me-5 ${
@@ -163,7 +146,7 @@ export default function StateList({ dataList, searchValue, objectState }) {
         ))}
       </ul>
       <StateMap displayMap={dataList} centerPointOfMap={searchValue}>
-        <MarkerF position={{ ...markerData }} />
+        {getMarkers}
       </StateMap>
     </div>
   );
