13f2d30a0290fd83bd1d9d9048f3c922c6a3f6ca
Bishop Shaw III <prototype124@yahoo.com>
2024-03-03 20:13:50 -0700

    fixed some bugs dealing with the list data links

--- src/components/AirlineListDataLinks.js
+++ src/components/AirlineListDataLinks.js
@@ -1,61 +1,45 @@
-import React from "react";
-
 import AirlineDataModal from "./AirlineDataModal";
+import ThumbsUpIcon from "./ThumbsUpIcon";
 
-export default function AirlineListDataLinks({ airlineNameData }) {
-  return (
-    <div className="bg-light">
-      <div className="container">
-        <div
-          style={{
-            margin: 0,
-          }}
-        >
-          <h1 className="text-center py-3 m-0">
-            Explore Your Favorite Airline
-          </h1>
-        </div>
-        <div
-          className=" mb-0 py-2"
-          style={{
-            marginLeft: "0",
-            marginRight: "0",
-            display: "grid",
-            gridTemplateColumns: "repeat(2, auto)",
-            justifyItems: "center",
-            width: "68%",
-            margin: "auto",
-          }}
-        >
-          {airlineNameData.map((airline, index) => (
-            <div
-              style={{
-                width: "96%",
-              }}
-              key={airline._id}
-              className="Links py-2 mb-3 border border-1 border-dark shadow rounded"
-            >
-              <h2
-                className={`airlineNameLink Link-${index + 1} py-2`}
-                data-bs-toggle="modal"
-                data-bs-target={`#airlineModalData-${airline._id}`}
-                style={{
-                  cursor: "pointer",
-                  width: "100%",
-                  paddingLeft: "25%",
-                }}
-              >
-                {airline.name}
-              </h2>
+const styles = {
+  width: "96%",
+  position: "relative",
+  cursor: "pointer",
+  background: "#fff",
+};
 
-              <AirlineDataModal
-                airlines={airline}
-                id={`airlineModalData-${airline._id}`}
-              />
-            </div>
-          ))}
-        </div>
-      </div>
+export default function AirlineListDataLinks({ airlineNameData, icons }) {
+  const create_airline_links = airlineNameData.map((airline, index) => (
+    <div
+      style={styles}
+      key={`${airline.name}-index`}
+      className={`Links py-2 mb-3 border border-primary rounded airlineNameLink Link-${
+        index + 1
+      } py-2`}
+      data-bs-toggle="modal"
+      data-bs-target={`#airlineModalData-${airline._id}`}
+    >
+      <h2
+        style={{
+          maxWidth: "100%",
+          textAlign: "center",
+        }}
+      >
+        {airline.name}
+      </h2>
+
+      {/* displays thumbs up icons with valid airport code look up */}
+      {icons[index] && <ThumbsUpIcon />}
+      <AirlineDataModal
+        airlines={airline}
+        id={`airlineModalData-${airline._id}`}
+      />
     </div>
+  ));
+
+  return (
+    <section>
+      <div className="airline-divs mb-0 py-4">{create_airline_links}</div>
+    </section>
   );
 }
