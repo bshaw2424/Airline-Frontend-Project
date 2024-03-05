257d7ce6522b5c563bace2f585f651672a148d00
Bishop Shaw III <prototype124@yahoo.com>
2024-03-04 20:55:10 -0700

    modified component to reset if wrong value input. Still working through bugs to get it working correct

--- src/components/AirlineStateSearch.js
+++ src/components/AirlineStateSearch.js
@@ -1,12 +1,15 @@
 import React, { useEffect } from "react";
 import StateList from "./StateList";
 import AirlineSearchResultsDisplay from "./AirlineSearchResultsDisplay";
+import Error from "./Error";
 
 export default function AirlineStateSearch({
   airlineSearch,
   targetCategoryValue,
   internationalSearchValue,
   selectOptionValue,
+  message,
+  messageDiv,
   isScrolled,
 }) {
   useEffect(() => {
@@ -33,43 +36,68 @@ export default function AirlineStateSearch({
     return getList.filter(arrayList => arrayList.codes.length !== 0).length;
   };
 
+  const getSound = () => {
+    const airline = airlineSearch
+      .map(item =>
+        item.destinations
+          .filter(item => item.international === "true")
+          .map(a => a.state.toLowerCase()),
+      )
+      .reduce((a, b) => a.concat(b), []);
+
+    return [...new Set(airline)];
+  };
+
+  const findValue = getSound().includes(targetCategoryValue.toLowerCase());
+
   const lengthOfDestinations = getListOfDestinations();
 
   return (
-    <article className="mt-5">
+    <article className={lengthOfDestinations === 0 ? "mt-0" : "mt-5"}>
       <div id="stateDestinationMap">
         <div className="d-flex justify-content-evenly flex-column">
-          {selectOptionValue === "state" && lengthOfDestinations !== 0 && (
+          {lengthOfDestinations === 0 && (
+            <Error message={message} messageDiv={messageDiv} />
+          )}
+
+          {!findValue && lengthOfDestinations !== 0 && (
             <>
-              <StateList
-                dataList={airlineSearch}
-                searchValue={targetCategoryValue}
-                objectState={getListOfDestinations()}
-                internationalSearchValue={internationalSearchValue}
-              />
-              <AirlineSearchResultsDisplay
-                selectOptionValue={selectOptionValue}
-                airlineNumber={getListOfDestinations()}
-                targetValue={targetCategoryValue}
-              />
+              {selectOptionValue === "state" && (
+                <>
+                  <StateList
+                    dataList={airlineSearch}
+                    searchValue={targetCategoryValue}
+                    objectState={getListOfDestinations()}
+                    internationalSearchValue={internationalSearchValue}
+                  />
+                  <AirlineSearchResultsDisplay
+                    selectOptionValue={selectOptionValue}
+                    airlineNumber={getListOfDestinations()}
+                    targetValue={targetCategoryValue}
+                  />
+                </>
+              )}
             </>
           )}
-          {selectOptionValue === "international" &&
-            lengthOfDestinations !== 0 && (
-              <>
-                <StateList
-                  dataList={airlineSearch}
-                  searchValue={targetCategoryValue}
-                  objectState={getListOfDestinations()}
-                  internationalSearchValue={internationalSearchValue}
-                />
-                <AirlineSearchResultsDisplay
-                  selectOptionValue={selectOptionValue}
-                  airlineNumber={getListOfDestinations()}
-                  targetValue={targetCategoryValue}
-                />
-              </>
-            )}
+          {findValue && lengthOfDestinations !== 0 && (
+            <>
+              {selectOptionValue === "international" && (
+                <>
+                  <StateList
+                    dataList={airlineSearch}
+                    searchValue={targetCategoryValue}
+                    objectState={getListOfDestinations()}
+                    internationalSearchValue={internationalSearchValue}
+                  />
+                  <AirlineSearchResultsDisplay
+                    selectOptionValue={selectOptionValue}
+                    airlineNumber={getListOfDestinations()}
+                    targetValue={targetCategoryValue}
+                  />
+                </>
+              )}
+            </>
+          )}
         </div>
       </div>
     </article>
