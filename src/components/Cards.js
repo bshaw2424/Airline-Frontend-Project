e7e14a56f5cf63ef649eada1e41b0c0b1c4e0e1b
Bishop Shaw III <prototype124@yahoo.com>
2024-05-16 20:18:22 -0700

    added bootstrap padding class

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,7 +1,7 @@
 function Cards({ children, id }) {
   return (
     <article
-      className="card d-flex flex-sm-column flex-md-row align-items-center pt-3 px-3 mb-4 list-flex"
+      className="card d-flex flex-sm-column flex-md-row align-items-center pt-3 px-5  px-lg-5 mb-4 list-flex"
       key={id}
     >
       {children}
