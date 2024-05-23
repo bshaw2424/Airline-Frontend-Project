eda5819a051cdd32cf66e415123518a68dde0925
Bishop Shaw III <prototype124@yahoo.com>
2024-05-22 19:45:18 -0700

    edit landing display with a new image

--- src/components/HomeDescription.js
+++ src/components/HomeDescription.js
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { SlLocationPin } from "react-icons/sl";
+
 const HomeDescription = () => {
   return (
     <section
@@ -9,27 +10,52 @@ const HomeDescription = () => {
       id="home-description-container"
       className="container d-flex justify-content-center align-items-center"
     >
-      <div>
-        <p
+      <div style={{ height: "100%" }}>
+        <div
           style={{
-            fontSize: "3rem",
-            color: "#333",
-            textTransform: "capitalize",
+            display: "flex",
+            height: "100%",
           }}
-          className="d-flex justify-content-center align-items-center"
         >
-          <span className="me-3 hero-icon">
-            <SlLocationPin />
-          </span>
-          Finding your next favorite travel destination awaits.
-        </p>
-        <div className=" mt-5 w-100 home-description-button-container">
-          <Link
-            className="btn btn-outline-dark mt-1 rounded-2 hero-btn"
-            to="/airlines"
+          <p
+            style={{
+              fontSize: "3rem",
+              color: "#333",
+              marginLeft: "1.5rem",
+              textTransform: "capitalize",
+              flex: 2,
+            }}
+            className="d-flex justify-content-center align-items-center"
           >
-            View Airlines
-          </Link>
+            <span className="me-3 hero-icon">
+              <SlLocationPin />
+            </span>
+            Finding your next favorite travel destination awaits.
+            <span className=" w-100 home-description-button-container">
+              <Link
+                className="btn btn-outline-dark mt-1 rounded-2 hero-btn"
+                to="/airlines"
+              >
+                View Airlines
+              </Link>
+            </span>
+          </p>
+          <div
+            className="image-container"
+            style={{
+              width: "100%",
+              height: "100%",
+              flex: 5.5,
+
+              alignSelf: "flex-end",
+            }}
+          >
+            <img
+              src="../airport.jpg"
+              alt=""
+              style={{ objectFit: "contain", height: "100%", width: "100%" }}
+            />
+          </div>
         </div>
       </div>
     </section>
