939d53914b607b4b026b9f3fb4ae9be49158f23b
Bishop Shaw III <prototype124@yahoo.com>
2024-06-13 20:32:47 -0700

    added what can i bring guide link to tsa webpage

--- src/components/AirlineFeature.js
+++ src/components/AirlineFeature.js
@@ -1,16 +1,32 @@
-import React from "react";
-
-const AirlineFeature = ({ icons, description, isLast }) => {
-  const marginClass = isLast ? "me-0" : "me-5";
+import { Link } from "react-router-dom";
+const AirlineFeature = ({ link }) => {
+  // const marginClass = isLast ? "me-0" : "me-5";
   return (
     <div
-      className={`rounded shadow-sm d-flex justify-content-center align-items-center flex-column airline-feature`}
+      className={`d-flex  justify-content-center align-items-center flex-column airline-feature`}
       style={{
-        fontSize: "1.3rem",
+        fontSize: "1.2em",
       }}
     >
-      <span>{icons}</span>
-      <p className="mt-3 px-3">{description}</p>
+      <h2
+        className="text-center pb-4 mb-4 border-bottom border-dark"
+        style={{ fontSize: "3rem" }}
+      >
+        Discover what items are <span className="text-danger">Prohibited</span>{" "}
+        and what items are <span className="text-success">Acceptable</span> for
+        carry-on and checked luggage before arriving at the airport.
+      </h2>
+      <span>
+        <Link
+          to={link}
+          target="_blank"
+          rel="noreferrer"
+          alt="TSA - What Can I Bring Resource List"
+          className="btn btn-lg rounded btn-dark"
+        >
+          What Can I Bring?
+        </Link>
+      </span>
     </div>
   );
 };
--- src/components/AirlineFeatureDescription.js
+++ src/components/AirlineFeatureDescription.js
@@ -1,30 +1,10 @@
-import { CiSearch, CiLocationOn } from "react-icons/ci";
-import { GiCommercialAirplane } from "react-icons/gi";
 import AirlineFeature from "./AirlineFeature";
 
 const AirlineFeatureDescription = () => {
   return (
-    <section
-      className="w-100"
-      id="feature-description"
-      style={{ background: "#f4f4f4" }}
-    >
+    <section className="w-100" id="feature-description">
       <div className="d-flex justify-content-evenly container py-5">
-        <AirlineFeature
-          icons={<CiSearch fontSize="2rem" />}
-          description="Search domestic, international, and seasonal destinations"
-          isLast={false}
-        />
-        <AirlineFeature
-          icons={<GiCommercialAirplane fontSize="2rem" />}
-          description="10 largest airlines operating in the United States."
-          isLast={false}
-        />
-        <AirlineFeature
-          icons={<CiLocationOn fontSize="2rem" />}
-          description="Search by airport code or state / country *state search relative to United States*"
-          isLast={true}
-        />
+        <AirlineFeature link="https://www.tsa.gov/travel/security-screening/whatcanibring/all" />
       </div>
     </section>
   );
